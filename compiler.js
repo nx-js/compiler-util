e9fae33c013210f18df395a12f267316a923448f
Miklos Bertalan <miklos.bertalan@risingstack.com>
2016-10-28 15:37:41 +0200

    perf: cache functions

--- compiler.js
+++ compiler.js
@@ -5,6 +5,9 @@ module.exports = {
   compileExpression
 }
 
+const expressionCache = new Map()
+const codeCache = new Map()
+
 function compileExpression (src, sandbox) {
   if (typeof src !== 'string') {
     throw new TypeError('first argument must be a string')
@@ -12,13 +15,16 @@ function compileExpression (src, sandbox) {
   if (typeof sandbox !== 'object') {
     throw new TypeError('second argument must be an object')
   }
-
-  sandbox = new Proxy(sandbox, {get, has})
-  const expression = `
-  try { with (sandbox) { return ${src} } } catch (err) {
-    if (!(err instanceof ReferenceError || err instanceof TypeError)) throw err
-  }`
-  return new Function('sandbox', expression).bind(sandbox, sandbox) // eslint-disable-line
+  sandbox = new Proxy(sandbox, {has})
+  let expression = expressionCache.get(src)
+  if (!expression) {
+    expression = new Function('sandbox',
+      `try { with (sandbox) { return ${src} } } catch (err) {
+        if (!(err instanceof ReferenceError || err instanceof TypeError)) throw err
+      }`)
+    expressionCache.set(src, expression)
+  }
+  return expression.bind(sandbox, sandbox) // eslint-disable-line
 }
 
 function compileCode (src, sandbox) {
@@ -28,18 +34,15 @@ function compileCode (src, sandbox) {
   if (typeof sandbox !== 'object') {
     throw new TypeError('second argument must be an object')
   }
-
-  sandbox = new Proxy(sandbox, {get, has})
-  return new Function('sandbox', `with (sandbox) { ${src} }`).bind(sandbox, sandbox) // eslint-disable-line
-}
-
-function get (target, key, receiver) {
-  if (key === Symbol.unscopables) {
-    return undefined
+  sandbox = new Proxy(sandbox, {has})
+  let code = codeCache.get(src)
+  if (!code) {
+    code = new Function('sandbox', `with (sandbox) { ${src} }`)
+    codeCache.set(src, code)
   }
-  return Reflect.get(target, key, receiver)
+  return code.bind(sandbox, sandbox) // eslint-disable-line
 }
 
-function has (target, key) {
+function has () {
   return true
 }
