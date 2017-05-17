625acaf209e4f3d39518bb076f2648a019a511e9
Miklos Bertalan <miklos.bertalan@risingstack.com>
2017-05-17 14:58:57 +0200

    perf(rawExpression): improve performance for simple expressions

--- src/rawCompiler.js
+++ src/rawCompiler.js
@@ -1,4 +1,22 @@
+const isSimple = /^(\w|\.)+$/
+
 export function compileRawExpression (src) {
+  return isSimple.test(src) ? compileSimpleExpression(src) : compileComplexExpression(src)
+}
+
+function compileSimpleExpression (src) {
+  return new Function('context', 'tempVars', // eslint-disable-line
+    `if (tempVars) {
+      try { return tempVars.${src} } catch (err) {
+        if (!(err instanceof TypeError)) throw err
+      }
+    }
+    try { return context.${src} } catch (err) {
+      if (!(err instanceof TypeError)) throw err
+    }`)
+}
+
+function compileComplexExpression (src) {
   return new Function('context', 'tempVars', // eslint-disable-line
     `const sandbox = $nxCompileToSandbox(context, tempVars)
     try { with (sandbox) { return ${src} } } catch (err) {
